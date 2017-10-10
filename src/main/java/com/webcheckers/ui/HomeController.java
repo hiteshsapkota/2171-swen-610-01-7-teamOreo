46b35b821049d8edbac5ed8fbc76f2ba0507b78e
hxs1943 <hiteshsapkota@gmail.com>
2017-10-10 16:16:26 -0400

    Added HomeController for the changes in home.ftl

--- src/main/java/com/webcheckers/ui/HomeController.java
+++ src/main/java/com/webcheckers/ui/HomeController.java
@@ -3,6 +3,7 @@ package com.webcheckers.ui;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.webcheckers.model.OnlinePlayers;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -14,11 +15,23 @@ import spark.TemplateViewRoute;
  * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
  */
 public class HomeController implements TemplateViewRoute {
-
+  public static String PLAYERSTATUS="currentPlayer";
+  public static String PLAYERSLIST="onlinePlayers";
+  public static String PLAYERNAME="playerName";
+  public static String GAMETITLE="title";
   @Override
   public ModelAndView handle(Request request, Response response) {
     Map<String, Object> vm = new HashMap<>();
     vm.put("title", "Welcome!");
+    String currentUsername=request.session().attribute("username");
+    if(currentUsername==null)
+      vm.put(PLAYERSTATUS,false);
+    else
+    {
+      vm.put(PLAYERSTATUS,true);
+      vm.put(PLAYERNAME,currentUsername);
+      vm.put(PLAYERSLIST,JsonUtils.toJson(OnlinePlayers.onlineList));
+    }
     return new ModelAndView(vm , "home.ftl");
   }
 
