9c0349f45934dbb05ae9a17913f273c45c70bd1c
dishantkaushik <dxk3597@g.rit.edu>
2017-10-18 20:52:16 -0400

    Added test case for PostSignInController and OnlinePlayersTest classes. Implemented 3 tests for PostSignInController.

---
+++ src/test/java/com/webcheckers/model/OnlinePlayersTest.java
@@ -0,0 +1,10 @@
+package com.webcheckers.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class OnlinePlayersTest {
+
+}
