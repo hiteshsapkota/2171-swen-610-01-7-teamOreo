f240e2ad93c0e7898528bb1a45ec92b7f7bb5654
Sayantika Bhattacharya <sxb2606@g.rit.edu>
2017-10-24 22:43:22 -0400

    Created Test classes for Space and GetSignOutcontroller

--- src/test/java/com/webcheckers/model/Board/SpaceTest.java
+++ src/test/java/com/webcheckers/model/Board/SpaceTest.java
@@ -1,5 +1,6 @@
 package com.webcheckers.model.Board;
 
+import com.sun.org.apache.regexp.internal.RE;
 import com.webcheckers.model.OnlinePlayers;
 import org.junit.Test;
 
@@ -18,6 +19,32 @@ public class SpaceTest {
         assertEquals(RED,piece.getColor());
         assertEquals(SINGLE, piece.getType());
 
+    }
+
+    @Test
+    public void checkSetIdx(){
+        int targetId = 5;
+        Space space = new Space(7, true, null);
+        space.setCellIdx(targetId);
+        assertEquals(targetId, space.getCellIdx());
+    }
+
+    @Test
+    public void checkSetValid(){
+        boolean targetValid = false;
+        Space space = new Space(5, true, null);
+        space.setValid(targetValid);
+
+        assertFalse(space.isValid());
+    }
+
+    @Test
+    public void checkGetAndSetPiece(){
+        Piece targetPiece = new Piece(SINGLE, RED);
+        Space space = new Space(5, true, null);
+
+        space.setPiece(targetPiece);
+        assertEquals(targetPiece, space.getPiece());
     }
 
 }
