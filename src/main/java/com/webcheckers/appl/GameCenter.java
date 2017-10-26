aee27ab15ba6d2cdef05677704ac3d945ae54589
Licelot <ldm4213@rit.edu>
2017-10-26 17:39:23 -0400

    I have added a skeleton class for GameCenter

---
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -0,0 +1,29 @@
+package com.webcheckers.appl;
+
+import spark.Session;
+
+public class GameCenter {
+
+    public void getGame(Session session){
+
+        /**
+         * This function will get the game for the current user
+         */
+
+    }
+
+    public void createGame(String player1, String player2) {
+
+        /**
+         * This function will create a new game for the two players
+         */
+    }
+
+    public void endGame (Session session){
+        /**
+         * This function will end the game
+         */
+
+
+    }
+}
