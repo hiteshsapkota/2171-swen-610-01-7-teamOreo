fc0d756b3770a462d6d7ee41157a80e45e236b94
hxs1943 <hiteshsapkota@gmail.com>
2017-10-26 17:57:37 -0400

    WebCheckerGame class is created in model section

---
+++ src/main/java/com/webcheckers/model/WebCheckerGame.java
@@ -0,0 +1,16 @@
+package com.webcheckers.model;
+
+public class WebCheckerGame
+{
+
+    public WebCheckerGame(String player1,String player2)
+    {
+        /**
+         * It will creat new board with rows,spaces and pieces
+         * @param player1
+         * @param player2
+         */
+    }
+
+
+}
