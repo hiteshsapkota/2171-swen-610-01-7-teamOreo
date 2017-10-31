f7c1270365143b1e26fb08470ae777b304c5df3b
Sayantika Bhattacharya <sxb2606@g.rit.edu>
2017-10-31 18:32:27 -0400

    OnlinePlayers, GetSignInController and GetSignOutController classes are refactored

--- src/main/java/com/webcheckers/ui/Login/GetSignOutController.java
+++ src/main/java/com/webcheckers/ui/Login/GetSignOutController.java
@@ -5,7 +5,9 @@ import static com.webcheckers.model.Strings.HOME_VIEW;
 import static com.webcheckers.model.Strings.TITLE_ATTR;
 import static com.webcheckers.model.Strings.USERNAME_ATTR;
 import static com.webcheckers.model.Strings.WELCOME_TITLE;
+import static spark.Spark.halt;
 
+import com.webcheckers.appl.GameCenter;
 import com.webcheckers.model.OnlinePlayers;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,18 +20,28 @@ import spark.TemplateViewRoute;
  * GET controller for Signing out.
  */
 public class GetSignOutController implements TemplateViewRoute {
+  private final GameCenter gameCenter;
+
+  public GetSignOutController(final GameCenter gameCenter) {
+    this.gameCenter = gameCenter;
+
+  }
 
   @Override
   public ModelAndView handle(Request request, Response response) {
     Map<String, Object> vm = new HashMap<>();
     // Remove the player from the online list.
-    OnlinePlayers.onlineList
-        .remove(new OnlinePlayers(request.session().attribute("username"), true));
     // Remove the player from the session variable
+
     request.session().removeAttribute(USERNAME_ATTR);
+
+    gameCenter.logout(request.session());
     // Show the home view.
-    vm.put(TITLE_ATTR, WELCOME_TITLE);
-    vm.put(CURRENT_PLAYER_ATTR, false);
-    return new ModelAndView(vm, HOME_VIEW);
+    response.redirect("/");
+    halt();
+    return null;
+//    vm.put(TITLE_ATTR, WELCOME_TITLE);
+//    vm.put(CURRENT_PLAYER_ATTR, false);
+//    return new ModelAndView(vm, HOME_VIEW);
   }
 }
