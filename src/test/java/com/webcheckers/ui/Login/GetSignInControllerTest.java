6a536c65f744d8c98756bb22bfe73434cbe2c479
hiteshsapkota <hiteshsapkota@gmail.com>
2017-11-06 14:37:45 -0500

    Implemented GetSignInControllerTest Class

--- src/test/java/com/webcheckers/ui/Login/GetSignInControllerTest.java
+++ src/test/java/com/webcheckers/ui/Login/GetSignInControllerTest.java
@@ -1,13 +1,52 @@
 package com.webcheckers.ui.Login;
 
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.model.OnlinePlayers;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+
+import static com.webcheckers.model.Strings.SIGNIN_VIEW;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class GetSignInControllerTest {
 
   @Test
-  public void handle() throws Exception {
+  public void handle1(){
+    Request request = mock(Request.class);
+    Response response = mock(Response.class);
+    Session session = mock(Session.class);
+    OnlinePlayers players = mock(OnlinePlayers.class);
+    GameCenter gameCenter = mock(GameCenter.class);
+
+    when(request.session()).thenReturn(session);
+    when(session.attribute("user")).thenReturn(players);
+
+    GetSignInController getSignInController = new GetSignInController(gameCenter);
+
+    assertNull(getSignInController.handle(request, response));
   }
 
+  @Test
+  public void handle2(){
+    Request request = mock(Request.class);
+    Response response = mock(Response.class);
+    Session session = mock(Session.class);
+    GameCenter gameCenter = mock(GameCenter.class);
+
+    when(request.session()).thenReturn(session);
+    when(session.attribute("user")).thenReturn(null);
+
+    GetSignInController getSignInController = new GetSignInController(gameCenter);
+
+    assertEquals(SIGNIN_VIEW, getSignInController.handle(request, response).getViewName());
+  }
 }
