0a8fa2edba94356ece56c700413d3160f6a3ec3d
hiteshsapkota <hiteshsapkota@gmail.com>
2017-11-06 14:34:36 -0500

    Implemented BoardTest Class

--- src/test/java/com/webcheckers/model/Board/BoardTest.java
+++ src/test/java/com/webcheckers/model/Board/BoardTest.java
@@ -2,12 +2,23 @@ package com.webcheckers.model.Board;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BoardTest {
+  private Board board;
+
+  @Before
+  public void start(){
+    board = new Board();
+  }
 
   @Test
   public void getRow() throws Exception {
+    for (int i = 0; i < 8; i++) {
+      assertEquals(i, board.getRow(i).getIndex());
+    }
+
   }
 
   @Test
