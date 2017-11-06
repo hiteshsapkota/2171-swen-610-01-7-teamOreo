e7b5590ab63f64cf3ba5c69c9a03d7da78ce1e86
Sayantika Bhattacharya <sxb2606@g.rit.edu>
2017-11-06 14:14:23 -0500

    Created SpaceTest, MessageTest, MovementTest, OnlinePlayersTest classes

--- src/test/java/com/webcheckers/model/OnlinePlayersTest.java
+++ src/test/java/com/webcheckers/model/OnlinePlayersTest.java
@@ -2,32 +2,41 @@ package com.webcheckers.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class OnlinePlayersTest {
+  private OnlinePlayers onlinePlayers;
+  private String test = "test";
+
+  @Before
+  public void start(){
+    onlinePlayers = new OnlinePlayers(test);
+  }
 
   @Test
   public void getName() throws Exception {
+    assertEquals(test, onlinePlayers.getName());
   }
 
   @Test
   public void isFree() throws Exception {
+    assertTrue(onlinePlayers.isFree());
   }
 
   @Test
   public void setFree() throws Exception {
+    onlinePlayers.setFree(false);
+    assertFalse(onlinePlayers.isFree());
   }
 
   @Test
   public void equals() throws Exception {
-  }
+    OnlinePlayers p2 = new OnlinePlayers(test);
+    assertTrue(onlinePlayers.equals(p2));
+    assertFalse(onlinePlayers.equals(null));
+    assertFalse(onlinePlayers.equals(test));
 
-//  @Test
-//  public void hashCode() throws Exception {
-//  }
-//
-//  @Test
-//  public void toString() throws Exception {
-//  }
+  }
 
 }
