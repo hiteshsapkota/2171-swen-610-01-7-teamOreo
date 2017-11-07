6141ea8f8cf376e458b99c46b0bea78b6afa1e05
hiteshsapkota <hiteshsapkota@gmail.com>
2017-11-06 20:05:54 -0500

    Inline comments added to the class Piece

--- src/main/java/com/webcheckers/model/Board/Piece.java
+++ src/main/java/com/webcheckers/model/Board/Piece.java
@@ -10,7 +10,7 @@ public class Piece {
    */
   public typeEnum type;
   public colorEnum color;
-
+//constructor
   public Piece(typeEnum type, colorEnum color) {
     this.type = type;
     this.color = color;
@@ -19,28 +19,50 @@ public class Piece {
   /**
    * Public methods
    */
+  /** Returns type of the piece(King or Single)
+   *
+   * @return typeEnum
+   */
   public typeEnum getType() {
     return type;
   }
 
+  /**
+   * Sets piece typr(King or Single)
+   * @param type
+   */
   public void setType(typeEnum type) {
     this.type = type;
   }
 
+  /**
+   *  Returns color of the piece
+   * @return colorEnum
+   */
   public colorEnum getColor() {
     return color;
   }
 
+  /**
+   * Sets color of the piece
+   * @param color
+   */
   public void setColor(colorEnum color) {
     this.color = color;
   }
 
+  /**
+   * declaration of enum for piece type
+   */
   public enum typeEnum {
     SINGLE,
     KING
 
   }
 
+  /**
+   * declaration of enum for piece color
+   */
   public enum colorEnum {
     RED,
     WHITE
