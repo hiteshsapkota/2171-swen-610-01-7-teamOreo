f46fcd4998bd6e2c39ed4f8cc261c2ae1c901b1e
hiteshsapkota <hiteshsapkota@gmail.com>
2017-11-06 20:19:17 -0500

    Inline comments are added to the class Message

--- src/main/java/com/webcheckers/model/Message.java
+++ src/main/java/com/webcheckers/model/Message.java
@@ -1,14 +1,28 @@
 package com.webcheckers.model;
 
+/**
+ * Message class sets type and text of the particular message
+ */
     public class Message {
-        public String text;
+    /**
+     * Attributes
+     */
+    public String text;
         public String type;
 
+    /**
+     * Constructor with input parameters
+     * @param text
+     * @param type
+     */
         public Message(String text, String type) {
             this.text = text;
             this.type = type;
         }
 
+    /**
+     * Default constructor
+     */
         public Message() {
 
         }
