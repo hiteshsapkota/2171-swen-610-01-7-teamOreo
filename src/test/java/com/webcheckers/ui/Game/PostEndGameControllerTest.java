8197167336365ea818a295b0b8048123ed6b4707
hiteshsapkota <hiteshsapkota@gmail.com>
2017-11-12 18:36:59 -0500

    Test class for PostEndGameController class created

---
+++ src/test/java/com/webcheckers/ui/Game/PostEndGameControllerTest.java
@@ -0,0 +1,27 @@
+package com.webcheckers.ui.Game;
+
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.ui.Login.GetSignOutController;
+import org.junit.Test;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PostEndGameControllerTest {
+    @Test
+    public void handle1(){
+        Request request = mock(Request.class);
+        Response response = mock(Response.class);
+        Session session = mock(Session.class);
+        GameCenter gameCenter = mock(GameCenter.class);
+        when(request.session()).thenReturn(session);
+        PostEndGameController postEndGameController = new PostEndGameController(gameCenter);
+
+        assertNull(postEndGameController.handle(request, response));
+    }
+
+}
