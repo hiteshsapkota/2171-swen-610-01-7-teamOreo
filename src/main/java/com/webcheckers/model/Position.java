2a2f9350ccaead93e6e6017eddc2a9e5486c37b1
dishantkaushik <dxk3597@g.rit.edu>
2017-11-11 23:12:22 -0500

    Created a class for positions

---
+++ src/main/java/com/webcheckers/model/Position.java
@@ -0,0 +1,31 @@
+package com.webcheckers.model;
+
+class Position{
+  private int x;
+  private int y;
+
+  Position(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Position position = (Position) o;
+
+    return x == position.x && y == position.y;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = x;
+    result = 31 * result + y;
+    return result;
+  }
+}
