3e63299d01e6f08510a8111aa5083846f6e003ea
dishantkaushik <dxk3597@g.rit.edu>
2017-11-11 23:17:25 -0500

    Added a post method to end the game and redirect the user to the home screen.

---
+++ src/main/java/com/webcheckers/ui/Game/PostEndGameController.java
@@ -0,0 +1,31 @@
+package com.webcheckers.ui.Game;
+
+import static spark.Spark.halt;
+
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.model.OnlinePlayers;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateViewRoute;
+
+public class PostEndGameController implements TemplateViewRoute {
+  private final GameCenter gameCenter;
+
+  public PostEndGameController(final GameCenter gameCenter) {
+    this.gameCenter = gameCenter;
+  }
+
+
+  @Override
+  public ModelAndView handle(Request request, Response response) {
+    String user = ((OnlinePlayers)request.session().attribute("user")).getName();
+    if(gameCenter.getGame(user) != null){
+      gameCenter.removeGame(user);
+    }
+    response.redirect("/");
+    halt();
+    return null;
+  }
+}
